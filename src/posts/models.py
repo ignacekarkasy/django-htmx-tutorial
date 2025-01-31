1ea7cb68c73ae9dee35eac059fcd70b4dfa3a959
Ignace Karkasy <felix.thiele@gmail.com>
2025-01-31 19:45:10 +0100

    created a Post model, made migrations and registed it in the admin models

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=500)
+    image = models.URLField(max_length=500)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    id = models.UUIDField(primary_key=True, editable=False)
