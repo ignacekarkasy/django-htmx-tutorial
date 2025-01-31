a342157030af78ea532bc7be5d697a79cfa855fc
Ignace Karkasy <felix.thiele@gmail.com>
2025-01-31 20:35:15 +0100

    created a model form view for post creation

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,10 +1,22 @@
+from django import forms
+from django.forms.models import ModelForm
 from django.shortcuts import render
+
 from .models import Post
 
 def home_view(request, *args, **kwargs):
     posts = Post.objects.all()
     return render(request, 'posts/home.html', {'posts':posts})
 
+class PostCreateForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'image', 'body']
+        labels = {'title': 'Title', 'image': 'Image', 'body': 'Caption'}
+        widgets = {
+            'body': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Add a Caption...', 'class': 'font1 text-4xl'}),
+        }
+
 def post_create_view(request, *args, **kwargs):
-    posts = Post.objects.all()
-    return render(request, 'posts/post_create.html', {'posts':posts})
+    form = PostCreateForm()
+    return render(request, 'posts/post_create.html', {'form':form})
